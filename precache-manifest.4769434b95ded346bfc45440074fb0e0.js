766c7452d93266c4031df2eb7e23eaf17a09454e
Islam <islamkaawad@gmail.com>
2022-12-27 15:46:37 +0600

    Updates

---
+++ precache-manifest.4769434b95ded346bfc45440074fb0e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eb3c3df8fbe6c79c3dd",
+    "url": "/reactTodo/static/css/main.55c01265.chunk.css"
+  },
+  {
+    "revision": "2eb3c3df8fbe6c79c3dd",
+    "url": "/reactTodo/static/js/main.2eb3c3df.chunk.js"
+  },
+  {
+    "revision": "e330dcf3d23ad7892912",
+    "url": "/reactTodo/static/js/1.e330dcf3.chunk.js"
+  },
+  {
+    "revision": "c738b07d86f4db702dc0",
+    "url": "/reactTodo/static/js/runtime~main.c738b07d.js"
+  },
+  {
+    "revision": "4eac2b2293d1f7e0949c81072ac40b24",
+    "url": "/reactTodo/index.html"
+  }
+];
